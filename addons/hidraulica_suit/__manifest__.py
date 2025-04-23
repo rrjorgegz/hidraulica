9212cd7ff91d322889cee15b2cf4fa3cff8836c1
Rafael Rubén Jorge González <rrjorgegz@gmail.com>
2025-04-23 16:47:04 -0400

    [FIX] change tag odoo 17.0

--- addons/hidraulica_suit/__manifest__.py
+++ addons/hidraulica_suit/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Suit Hidraulica",
+    "name": "Suit Hidráulica",
     "summary": "Short (1 phrase/line) summary of the module's purpose",
     "author": "ADATECS.surl",
     "website": "https://www.desoft.cu",
